6f55aec450f32c5be2f45918ae459dfe1a7413d8
tmddk9445 <tmddk9445@naver.com>
2023-03-21 01:45:16 +0900

    20230321 TUE front-end REACT

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 const topics = [
-  {id:1, title:'html', body:'html is ...'}
-  {id:2, title:'css', body:'css is ...'}
+  {id:1, title:'html', body:'html is ...'},
+  {id:2, title:'css', body:'css is ...'},
   {id:3, title:'js', body:'js is ...'}
 ]
 
