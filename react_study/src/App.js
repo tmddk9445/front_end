911f098a8009a993742a085b2ad69018693cfa34
tmddk9445 <tmddk9445@naver.com>
2023-03-21 01:24:37 +0900

    20230321 TUE REACT

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,10 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  console.log('props', props.title)
+  return (
+    <div>
+      <header>
+        <h1>
+          <a href="/">{props.title}</a>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      <Header title="WEB"></Header>
+      <Nav></Nav>
+      <Article title="Welcome" body="Hello, web"></Article>
     </div>
   );
 }
