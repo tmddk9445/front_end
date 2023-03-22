fa2d8a8ab7ca55b1d16c27f6548af643d9d69982
tmddk9445 <tmddk9445@naver.com>
2023-03-22 14:14:09 +0900

    20230322 WED REACT

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {useState} from 'react';
 
 function Header(props) {
-  console.log('props', props.title)
   return (
       <header>
         <h1>
@@ -44,31 +43,132 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return <article>
+      <h2>Create</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+}
+
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" value={title} onChange={event => {
+            setTitle(event.target.value);
+          }}/>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body" value={body} onChange={event => {
+            setBody(event.target.value);
+          }}></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Update"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
 
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'js', body:'js is ...'}
-  ]
+  ]);
 
   let content = null;
+  let contextControl = null;
+
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web"></Article>
+
   } else if (mode === 'READ') {
+
     let title, body = null;
     for (let i = 0; i < topics.length; i++) {
-      console.log(topics[i].id, id);
+
       if(topics[i].id === id) {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>
-  }
+    contextControl = <li><a href={'/update/'+id} onClick={event => {
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+
+  } else if (mode === 'CREATE') {
+
+    content = <Create onCreate={(_title, _body) => {
+
+      const newTopic = {id: nextId, title: _title, body: _body}
+      const newTopics = [...topics]
+
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode("READ");
+      setId(nextId);
+      setNextId(nextId + 1);
+
+    }}></Create>
+    
+  } else if (mode === 'UPDATE') {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body) => {
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body}
+      for(let i=0; i < newTopics.length; i++) {
+        if(newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode("READ");
+    }}></Update>
+  } 
 
   return (
     <div>
@@ -80,6 +180,15 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <ul>
+        <li>
+          <a href='/create' onClick={event => {
+            event.preventDefault();
+            setMode('CREATE');
+          }}>CREATE</a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
